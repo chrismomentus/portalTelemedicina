16dfa9709b1da5a94d511591181ec45859367d13
Chris Chinedozie <chrismomentus@gmail.com>
2021-06-27 02:09:48 -0300

    final release

--- api_telemed/urls.py
+++ api_telemed/urls.py
@@ -1,16 +1,3 @@
-from django.urls import path, include, re_path
-from . import views
-from rest_framework import routers
-from .views import *
+from django.urls import path
 
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'products', productViewset, basename="products")
-router.register(r'orders', orderViewset, basename="orders")
-
-
-urlpatterns = [
-    path('home/', views.home, name="home"),
-    path('product/(?P<pk>[0-9]+)/', Delete_Put_Product.as_view(),name='Delete_Put_product'),
-    path('', include(router.urls)),
-]
+urlpatterns = []
--- project_telemedicina/urls.py
+++ project_telemedicina/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
+from api_telemed.views import *
 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -18,14 +19,16 @@ class UserViewSet(viewsets.ModelViewSet):
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'products', productViewset, basename="products")
+router.register(r'orders', orderViewset, basename="orders")
+
+
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api/', include('api_telemed.urls')),
-    path('rest-auth/', include('rest_auth.urls')),
-    #path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    path('signup/', include('rest_auth.registration.urls')),
-    path('', include(router.urls)),
-    #path('api-auth/', include('rest_framework.urls', namespace="rest_framework")),
+    path('signup/', include('rest_auth.registration.urls')),    
     path('login/', include('rest_framework.urls', namespace="rest_framework")),
+    path('product/(?P<pk>[0-9]+)/', Delete_Put_Product.as_view(),name='Delete_Put_product'),
 ]
